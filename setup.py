1bba550d6ee18bda148087bc37b467b8daf458a8
Etienne Mueller <etienne.mueller@outlook.com>
2022-06-13 21:39:41 +0200

    preparing for PyPI

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='convert2snn',
-    version='0.0.1',
+    version='0.0.2',
     author='Etienne Mueller',
     author_email='etienne.mueller@tum.de',
     description='Convert convention to spiking neural networks.',
